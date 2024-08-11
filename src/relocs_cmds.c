3baeb02cae697bb1840e0ecc31e3a0c97ef9ea02
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2024-08-10 22:50:27 -0700

    Refactoring: move redirect_reloc() to a separate C file

---
+++ src/relocs_cmds.c
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "basic_defs.h"
+#include "elf_utils.h"
+#include "cmds.h"
+
+static int
+redirect_reloc(struct elf_file_info *nfo, const char *sym1, const char *sym2)
+{
+   Elf_Ehdr *h = (Elf_Ehdr*)nfo->vaddr;
+   Elf_Sym *s1 = get_symbol_by_name(h, sym1, NULL);
+   Elf_Sym *s2 = get_symbol_by_name(h, sym2, NULL);
+
+   if (!s1) {
+      fprintf(stderr, "Cannot find symbol '%s'\n", sym1);
+      return 1;
+   }
+
+   if (!s2) {
+      fprintf(stderr, "Cannot find symbol '%s'\n", sym2);
+      return 1;
+   }
+
+   Elf_Shdr *sections = (Elf_Shdr *) ((char *)h + h->e_shoff);
+   for (uint32_t i = 0; i < h->e_shnum; i++) {
+      Elf_Shdr *s = sections + i;
+      if (s->sh_type == SHT_REL || s->sh_type == SHT_RELA) {
+         redirect_rel_internal(h, s, s1, s2);
+      }
+   }
+
+   return 0;
+}
+
+REGISTER_CMD(
+   redirect_reloc,
+   "--redirect-reloc",
+   "<sym1> <sym2>",
+   2,
+   &redirect_reloc
+)
